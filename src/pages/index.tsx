9ef4e201b6a492cac021dc6d1f0a18511db5f5b8
MathPSantos <matheus.psantos2016@gmail.com>
2021-05-05 20:52:23 -0300

    chore: add typescript to project

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Next Auth</title>
+      </Head>
+
+      <h1>Hello World</h1>
+    </>
+  );
+}
