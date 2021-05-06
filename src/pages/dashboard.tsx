095d04b999d4660fd824c2a051637ba3ed368b5e
MathPSantos <matheus.psantos2016@gmail.com>
2021-05-06 19:23:23 -0300

    feat: create withSSRAuth function to handle auth in SSR

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,6 +1,8 @@
 import Head from "next/head";
 
 import { useAuth } from "../contexts/AuthContext";
+import { setupAPIClient } from "../services/api";
+import { withSSRAuth } from "../utils/withSSRAuth";
 
 export default function Dashboard() {
   const { user } = useAuth();
@@ -15,3 +17,13 @@ export default function Dashboard() {
     </>
   );
 }
+
+export const getServerSideProps = withSSRAuth(async (ctx) => {
+  const apiClient = setupAPIClient(ctx);
+
+  const response = await apiClient.get("/me");
+
+  return {
+    props: {},
+  };
+});
